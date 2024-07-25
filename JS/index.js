b9959f6a9bea716eae75f32b9e62969dd940d77f
TofSas22 <tsasman13@gmail.com>
2024-07-25 23:03:46 +0200

    progress

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,37 @@
+document.getElementById("refresh").addEventListener('click', fetchContacts);
+document.getElementById("addContact").addEventListener('click', addContact);
 
-function fetchContacts(){
+function fetchContacts() {
     fetch(rootPath + "controller/get-contacts/")
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        //displayOutput(data);
-        console.log(data);
-    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            displayOutput(data);
+            //console.log(data);
+        })
+}
+
+function displayOutput(data) {
+    output = "<table>";
+
+    for (a in data) {
+        output += `
+            <tr>
+                <td><img src="${rootPath}controller/uploads/${data[a].avatar}" width="40"></td>
+                <td>
+                    <h5>${data[a].firstname}</h5>
+                </td>
+                <td>
+                    <h5>${data[a].lastname}</h5>
+                </td>
+            </tr>
+        `
+    }
+    output += "</table>";
+    document.getElementById("table").innerHTML = output;
+}
+
+function addContact() {
+    window.open("add-contact.html", "_self");
 }
